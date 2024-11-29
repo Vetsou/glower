4b0748d0d9bdfe1a01581ad1ea7dd7633b98e2ed
Vetsou <152625239+Vetsou@users.noreply.github.com>
2024-11-29 02:59:46 +0100

    Add initial DB implementation (#1)

---
+++ model/db.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func setupDatabase() {
+	dsn := os.Getenv("DB_DSN")
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error setting up database: " + err.Error())
+	}
+
+	conn, err := db.DB()
+	if err != nil {
+		log.Fatal("Error setting up database connection pool: " + err.Error())
+	}
+
+	conn.SetConnMaxIdleTime(5)
+	conn.SetMaxIdleConns(10)
+	conn.SetConnMaxLifetime(5 * time.Minute)
+
+	DB = db
+}
+
+func InitDatabase() {
+	setupDatabase()
+
+	err := DB.AutoMigrate(&Flower{}, &Stock{})
+	if err != nil {
+		log.Fatal("Error during DB auto migrate: " + err.Error())
+	}
+}

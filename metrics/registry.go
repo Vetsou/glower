8bbf278802c006c90cb781b20be00c0c85677a4c
Vetsou <virees@posteo.net>
2025-04-18 04:20:22 +0200

    Update metrics

--- metrics/registry.go
+++ metrics/registry.go
@@ -2,6 +2,16 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
+var (
+	HTTPTotalRequest = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "endpoint"},
+	)
+)
+
 func CreateRegistry() *prometheus.Registry {
 	reg := prometheus.NewRegistry()
 	reg.MustRegister(HTTPTotalRequest)
